91222de5c9ebc444ffb73694e57a6ae94cdd493e
MauricioHidani <mauricio.hidani.work@gmail.com>
2024-10-12 12:35:15 -0300

    refactor: use banner app props

--- src/main/java/br/com/alura/challenges/currency/converter/utils/BannerUtil.java
+++ src/main/java/br/com/alura/challenges/currency/converter/utils/BannerUtil.java
@@ -1,5 +1,7 @@
 package br.com.alura.challenges.currency.converter.utils;
 
+import br.com.alura.challenges.currency.converter.models.app.Banner;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,28 +11,37 @@ public class BannerUtil {
 	private final String BANNER_ARCHIVE = "banner.txt";
 	private final String BANNER_STD = "Alura One Challenge - Currency Converter";
 
-	public String load() {
+	public String load(Banner bannerConfig) {
+		var builder = new StringBuilder();
+
 		try {
 			var inputStream = BannerUtil.class.getClassLoader().getResourceAsStream(BANNER_ARCHIVE);
-			var builder = new StringBuilder();
-
 			if (inputStream != null) {
 				try (var reader = new BufferedReader(new InputStreamReader(inputStream))) {
 					var line = "";
 					while ((line = reader.readLine()) != null) {
-						builder.append(line + '\n');
+						builder.append(line);
+						builder.append('\n');
 					}
-					return builder.toString();
-
 				} catch (IOException e) {
-					return BANNER_STD;
+					builder.append(BANNER_STD);
 				}
 			} else {
-				return BANNER_STD;
+				builder.append(BANNER_STD);
 			}
 		} catch (NullPointerException e) {
-			return BANNER_STD;
+			builder.append(BANNER_STD);
 		}
+
+		if (bannerConfig != null) {
+			builder.append(bannerConfig.title());
+			builder.append('\s');
+			builder.append(bannerConfig.version());
+			builder.append('\n');
+			builder.append(bannerConfig.since());
+			builder.append('\n');
+		}
+		return builder.toString();
 	}
 
 }
