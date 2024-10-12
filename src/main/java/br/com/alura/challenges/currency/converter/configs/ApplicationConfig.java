b47b66a891b7856451ae04ee734cd8ff99c3dfdb
MauricioHidani <mauricio.hidani.work@gmail.com>
2024-10-12 11:18:29 -0300

    feat: add depd banner util

--- src/main/java/br/com/alura/challenges/currency/converter/configs/ApplicationConfig.java
+++ src/main/java/br/com/alura/challenges/currency/converter/configs/ApplicationConfig.java
@@ -1,5 +1,11 @@
 package br.com.alura.challenges.currency.converter.configs;
 
+import br.com.alura.challenges.currency.converter.utils.BannerUtil;
+
 public class ApplicationConfig {
+
+	public BannerUtil bannerUtil() {
+		return new BannerUtil();
+	}
 
 }
