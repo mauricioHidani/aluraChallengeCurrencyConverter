b7b1fc68d9d344f99e9deb7b61b33ae60f0bc8a2
MauricioHidani <mauricio.hidani.work@gmail.com>
2024-10-12 23:01:35 -0300

    feat: local date time parse util

---
+++ src/main/java/br/com/alura/challenges/currency/converter/utils/LocalDateTimeParseUtil.java
@@ -0,0 +1,35 @@
+package br.com.alura.challenges.currency.converter.utils;
+
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class LocalDateTimeParseUtil {
+
+	public static LocalDateTime convertRFC1123(final String target) {
+		if (target.isBlank()) {
+			throw new IllegalArgumentException("Não é possivel realizar a operação com uma data inválida.");
+		}
+
+		try {
+			var formatter = DateTimeFormatter.RFC_1123_DATE_TIME;
+			var zonedDateTime = ZonedDateTime.parse(target, formatter);
+			return zonedDateTime.withZoneSameInstant(ZoneId.systemDefault()).toLocalDateTime();
+		} catch (DateTimeParseException e) {
+			System.out.println("ERROR: Não é possivel converter a data no formato especificado.");
+		} catch (DateTimeException e) {
+			System.out.println("ERROR: Não é realizável a conversão da data não identificado seu local.");
+		}
+
+		return null;
+	}
+
+	public static String toComplete(final LocalDateTime dateTime) {
+		var fmt = DateTimeFormatter.ofPattern("EEEE, d MMMM yyyy");
+		return fmt.format(dateTime);
+	}
+
+}
